4d65c3a03575ce53359d57da45dc3836a3f609e2
Nonosthecrack <ferault.noa@gmail.com>
2025-12-11 16:52:22 +0100

    script npm 2

--- initialisation.bat
+++ initialisation.bat
@@ -1,11 +1,16 @@
-npm init -y
+@echo off
+echo Installation des librairies NPM...
 
+REM Installation des dépendances principales
 npm install nodemon
 npm install express
 npm install zod
 npm install bcrypt
 npm install jsonwebtoken
 
-drizzle:
+REM Installation de Drizzle ORM
 npm install drizzle-orm @libsql/client dotenv
 npm install -D drizzle-kit
+
+echo Installation terminee !
+pause
