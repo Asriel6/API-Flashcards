8cf60a3959f1c9956a3e96f9a759b8887a30adee
Ragot <Ragot@unicaen.fr>
2025-12-11 10:57:11 +0100

    db schema done

---
+++ src/db/schema.js
@@ -0,0 +1,92 @@
+import {sqliteTable, text, integer} from 'drizzle-orm/sqlite-core'
+import {randomUUID} from 'crypto'
+import { timeStamp } from 'console'
+import { boolean, date } from 'zod'
+
+export const personnalFlashCardsTable = sqliteTable('PersonnalFlashCards',{
+
+    userId: text()
+    .primaryKey()
+    .$defaultFn(()=> crypto.randomUUID())
+    .references(() => usersTable.id, {onDelete: 'cascade'}),
+
+    level: integer().notNull().default(1),
+
+    lastStudyDate: date(),
+
+    nextStudyDate: date(),
+
+    flashCardId: text()
+    .notNull()
+    .references(() => flashCardTable.id, {onDelete: 'cascade'})
+
+})
+
+export const flashCardTable = sqliteTable('FlashCards',{
+
+    id: text()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+
+    rectoText: text('rectoText',{length:255})
+    .notNull(),
+
+    rectoUrl: text('rectoUrl',{length:255}),
+
+    versoText: text('versoText',{length:255})
+    .notNull(),
+
+    versoUrl: text('versoUrl',{length:255}),
+
+    collectionId: text()
+    .notNull()
+    .references(() => collectionTable.id, {onDelete: 'cascade'}),
+    
+    ownerId: text()
+    .notNull()
+    .references(() => usersTable.id, {onDelete: 'cascade'})
+
+})
+
+export const usersTable = sqliteTable('Users',{
+
+    id: text()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+
+    email: text('email',{length : 255})
+    .notNull(),
+
+    password: text('password',{length : 255})
+    .notNull(),
+
+    name: text('name',{length : 255})
+    .notNull(),
+
+    surname: text('surname',{length : 255})
+    .notNull(),
+
+    isAdmin: boolean()
+    .notNull()
+
+})
+
+export const collectionTable = sqliteTable('Collection',{
+
+    id: text()
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+
+    ownerId: text()
+    .notNull()
+    .references(() => usersTable.id, {onDelete: 'cascade'}),
+
+    title: text('Title', {length : 255})
+    .notNull(),
+
+    description: text('description', {length : 255})
+    .notNull(),
+
+    visbility: text({enum:['public', 'private']})
+    .notNull()
+})
